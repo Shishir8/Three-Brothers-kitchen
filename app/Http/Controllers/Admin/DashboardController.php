e5000d2a477f36417360982d8530adefd6950d0d
unknown <shishirkarki18@gmail.com>
2020-10-07 21:16:36 +0545

    1. Layouts Fix, login redirect and logout manage 2. Show Slider With DataTables 3. Add New Slider for font page 4. Edit Slider 5. Delete Slider 6. Show Slider in Frontend 7. Category CRUD

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use App\Contact;
+use App\Item;
+use App\Reservation;
+use App\Slider;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Slider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $sliders = Slider::all();
+        return view('welcome',compact('sliders'));
+    }
+}
